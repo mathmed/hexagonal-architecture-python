8e3947f1391fb0d2e932a44f90e6be086a46d17a
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-08-29 11:54:35 -0300

    feat: hash password

--- src/core/usecases/create_user/create_user_usecase.py
+++ src/core/usecases/create_user/create_user_usecase.py
@@ -1,30 +1,37 @@
 from src.core.ports.primary import CreateUserPort, CreateUserParams
-from src.core.contracts import UserRepositoryContract, SaveUserParams
+from src.core.contracts import UserRepositoryContract, SaveUserParams, HashContract
+from typing import Dict
 
 class CreateUserUsecase(CreateUserPort):
 
     def __init__(
         self,
-        user_repository: UserRepositoryContract
+        user_repository: UserRepositoryContract,
+        hash_service: HashContract
     ):
         self.user_repository = user_repository
+        self.hash_service = hash_service
 
-    def create(self, params: CreateUserParams) -> str:
-
+    def create(self, params: CreateUserParams) -> Dict:
+        
         username_exists = self.user_repository.get('username', params.username, False)
         email_exists = self.user_repository.get('email', params.email, False)
 
         if username_exists or email_exists:
             return 'User already exists'
 
+        hashed_password = self.hash_service.encode(params.password)
+
         saved_user = self.user_repository.save(
             SaveUserParams(
                 params.username,
-                params.password,
+                hashed_password,
                 params.name,
                 params.email,
                 params.profile_image_url,
                 params.bio
             )
         )
-        return saved_user
+
+        del saved_user.password
+        return saved_user.__dict__
--- src/presentation/factories/create_user_factory.py
+++ src/presentation/factories/create_user_factory.py
@@ -1,9 +1,11 @@
 from src.core.usecases import CreateUserUsecase
 from src.core.services.repositories import UserRepository
+from src.core.services.hash import Hash
 from src.core.ports.primary import CreateUserPort
 from src.infra.database import PyMongoAdapter
 
 def create_user_factory() -> CreateUserPort:
     pymongo = PyMongoAdapter()
+    hash = Hash()
     user_repository = UserRepository(pymongo)
-    return CreateUserUsecase(user_repository)
+    return CreateUserUsecase(user_repository, hash)
