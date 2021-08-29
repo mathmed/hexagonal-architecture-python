b1f520996de475223d981e97bb8344a69dc4e838
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-08-29 12:12:19 -0300

    refactor: ajust hash service

--- src/core/services/hash/hash.py
+++ src/core/services/hash/hash.py
@@ -1,9 +1,10 @@
 from src.core.contracts import HashContract
 import os
 import hashlib
+import base64
 
 class Hash(HashContract):
     def encode(self, string: str) -> str:
         salt = os.getenv('PASSWORD_ENCRYPT_SALT')
-        string = hashlib.pbkdf2_hmac('sha256', string.encode('utf-8'), salt.encode(), 100000)
+        string = base64.b64encode(hashlib.pbkdf2_hmac('sha256', string.encode('utf-8'), salt.encode(), 100000)).decode('utf-8')
         return string
