6df7cad333aa8fb553c6ffe76909a68a0653bac2
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-08-29 11:35:03 -0300

    feat: add findone on pymongo

--- src/core/ports/secundary/database_port.py
+++ src/core/ports/secundary/database_port.py
@@ -5,3 +5,6 @@ class DatabasePort(ABC):
     @abstractmethod
     def save(self, table: str, params: Dict) -> Dict:
         raise Exception('Method not implemented')
+
+    def find_one(self, table: str, by: str, value: any) -> Dict:
+        raise Exception('Method not implemented')
--- src/infra/database/adapters/pymongo_adapter.py
+++ src/infra/database/adapters/pymongo_adapter.py
@@ -11,9 +11,16 @@ class PyMongoAdapter(DatabasePort):
         self.db = self.client[os.getenv('DB_DATABASE')]
     
     def _parse_result(self, result: str) -> Dict:
-        return loads(dumps(result, default=str))
+        return loads(dumps(result, default=str))        
 
     def save(self, table: str, params: Dict) -> Dict:
         table = self.db[table]
         table.insert_one(params).inserted_id
         return self._parse_result(params)
+
+    def find_one(self, table: str, by: str, value: any) -> Dict:
+        if by == 'id':
+            by = '_id'
+        table = self.db[table]
+        result = table.find_one({by:value})
+        return self._parse_result(result)
