7274cef2792f67069cb46bf9e7d5b376f518a79b
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-09-04 18:43:42 -0300

    refactor: use class instead variables

--- src/presentation/helpers/http_status_code.py
+++ src/presentation/helpers/http_status_code.py
@@ -3,7 +3,7 @@ import dataclasses as dc
 @dc.dataclass(unsafe_hash=True)
 class HttpStatusCode:
     OK: int = 200
-    CREATED: int = 21
+    CREATED: int = 201
     BAD_REQUEST: int = 400
     UNAUTHORIZED: int = 401
     INTERNAL_SERVER_ERROR: int = 500
