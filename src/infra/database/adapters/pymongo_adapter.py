473ce661b8073dc98087e277a3d5245e87aab986
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-09-04 19:43:37 -0300

    feat: use singleton

--- src/infra/database/adapters/pymongo_adapter.py
+++ src/infra/database/adapters/pymongo_adapter.py
@@ -6,21 +6,32 @@ import os
 
 
 class PyMongoAdapter(DatabasePort):
+
+    _instance = None
+    _client = None
+    _db = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self.client = MongoClient('mongodb://{}:{}/'.format(os.getenv('DB_HOST'), os.getenv('DB_PORT')))
-        self.db = self.client[os.getenv('DB_DATABASE')]
-    
+        if not self._client:
+            self._client = MongoClient('mongodb://{}:{}/'.format(os.getenv('DB_HOST'), os.getenv('DB_PORT')))
+            self._db = self._client[os.getenv('DB_DATABASE')]
+
     def _parse_result(self, result: str) -> Dict:
         return loads(dumps(result, default=str))        
 
     def save(self, table: str, params: Dict) -> Dict:
-        table = self.db[table]
+        table = self._db[table]
         table.insert_one(params).inserted_id
         return self._parse_result(params)
 
     def find_one(self, table: str, by: str, value: any) -> Dict:
         if by == 'id':
             by = '_id'
-        table = self.db[table]
+        table = self._db[table]
         result = table.find_one({by:value})
         return self._parse_result(result)
